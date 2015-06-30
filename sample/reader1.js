674799b5d55feee498b98e45d2c0775592be4c37
guozhaokui <guozhaokuiwang@gmail.com>
2015-06-30 09:33:45 +0800

    ..

--- sample/reader1.js
+++ sample/reader1.js
@@ -9,7 +9,10 @@ var biaodian = '，。：！“”‘’…、？（）《》　/';
 var ebiaodian = escape(biaodian).replace(/%/g,'\\');
 var regbiao = new RegExp('[+'+ebiaodian+']','g');
 buf = buf.replace(regbiao,' ');
+//空格也去掉
+buf = buf.replace(/[ ]*/g,'');
 
+//统计个数
 function clusterdt(){
 	this.dtnum=0;
 	this.smpdt={};
@@ -29,14 +32,14 @@ function clusterdt(){
 		var sorted = new Array(this.dtnum);
 		var i=0;
 		for(var k in this.smpdt){
-			sorted[i++]={k:k,v:this.smpdt[k]};
+			sorted[i++]=[k,this.smpdt[k]];
 		}
-		sorted.sort(function(a,b){return b.v-a.v;});
+		sorted.sort(function(a,b){return b[1]-a[1];});
 		//去掉少的
 		var cutnum=3;
 		var n=0;
 		for(; n<sorted.length; n++){
-			if(sorted[n].v<cutnum)
+			if(sorted[n][1]<cutnum)
 				break;
 		}
 		return sorted.slice(0,n);
@@ -45,28 +48,20 @@ function clusterdt(){
 		var sted = this.sort();
 		var outbuf= new Array(sted.length);
 		var i=0;
-		sted.forEach(function(v){outbuf[i++]=(v.k+' '+v.v);});
+		sted.forEach(function(v){outbuf[i++]=(v[0]+' '+v[1]);});
 		fs.writeFileSync(f,outbuf.join('\r\n'));	
 	}
 }
 
-function statword(buf,wlen,f){
+function statword(buf,wlen){
 	var words=new clusterdt();
 	var len = buf.length;
 	for(var i=0; i<len-wlen; i++){
 		var w = buf.slice(i,i+wlen);
 		i%1000==0?log(i/len):"";
-		if(w.indexOf(' ')<0)
-			words.adddt(w);
+		words.adddt(w);
 	}
-	words.save(f);
+	return words.sort();
 }
 
-statword(buf,1,'word1.txt');
-statword(buf,2,'word2.txt');
-statword(buf,3,'word3.txt');
-statword(buf,4,'word4.txt');
-statword(buf,5,'word5.txt');
-statword(buf,6,'word6.txt');
-statword(buf,7,'word7.txt');
-
+log(statword(buf,1));
