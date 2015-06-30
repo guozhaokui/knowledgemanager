a73ad8ce8b58e5bae7aba45d6f35001387a3fc0a
guozhaokui <guozhaokuiwang@gmail.com>
2015-06-30 10:28:22 +0800

    概率

--- sample/reader1.js
+++ sample/reader1.js
@@ -1,4 +1,4 @@
-var log = console.log;
+﻿var log = console.log;
 var fs = require('fs');
 
 var buf = fs.readFileSync('sdyxz.txt','utf8');
@@ -28,7 +28,8 @@ function clusterdt(){
 		this.smpdt={};
 		this.dtnum=0;
 	}
-	this.sort=function(){
+	//排序，去掉少的
+	this.sort=function(cutnum){
 		var sorted = new Array(this.dtnum);
 		var i=0;
 		for(var k in this.smpdt){
@@ -36,16 +37,18 @@ function clusterdt(){
 		}
 		sorted.sort(function(a,b){return b[1]-a[1];});
 		//去掉少的
-		var cutnum=3;
-		var n=0;
-		for(; n<sorted.length; n++){
-			if(sorted[n][1]<cutnum)
-				break;
+		if(cutnum>0){
+			var n=0;
+			for(; n<sorted.length; n++){
+				if(sorted[n][1]<cutnum)
+					break;
+			}
+			return sorted.slice(0,n);
 		}
-		return sorted.slice(0,n);
+		return sorted;
 	}
 	this.save=function(f){
-		var sted = this.sort();
+		var sted = this.sort(3);
 		var outbuf= new Array(sted.length);
 		var i=0;
 		sted.forEach(function(v){outbuf[i++]=(v[0]+' '+v[1]);});
@@ -53,15 +56,38 @@ function clusterdt(){
 	}
 }
 
-function statword(buf,wlen){
+function statword(buf,wlen,cutnum){
 	var words=new clusterdt();
 	var len = buf.length;
 	for(var i=0; i<len-wlen; i++){
 		var w = buf.slice(i,i+wlen);
-		i%1000==0?log(i/len):"";
+		//i%1000==0?log(i/len):"";
 		words.adddt(w);
 	}
-	return words.sort();
+	return words.smpdt;//words.sort(cutnum==null?3:cutnum);
 }
-
-log(statword(buf,1));
+//单个字的概率
+var word1 = statword(buf,1);
+/*
+word1.forEach(function(w){
+	w[1]/=buf.length;
+});
+*/
+//两个字的概率P(w1w2)
+var word2 = statword(buf,2);
+/*
+word2.forEach(function(w){
+})
+*/
+//计算在w1后面跟着w2的概率
+function getPW2(w1,w2){
+	var w=w1+w2;
+	var wn = word2[w];
+	log('wn='+wn+','+word1[w1]);
+	if(!wn) return 0;
+	return wn/word1[w1];
+}
+//计算是句子的概率
+log(getPW2('黄','蓉' ));
+log(getPW2('欧','阳' ));
+//w1 w2的概率
